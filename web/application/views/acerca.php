5ddb94fdebbd0dacc3972565be192670aa4151d6
Juan Vicente <jdanielvq6@gmail.com>
2024-09-17 09:01:28 -0400

    Se agrego la navegacion estre la nuevas rutas

--- web/application/views/acerca.php
+++ web/application/views/acerca.php
@@ -1,41 +1,5 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?>
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
-</head>
-<body>
 
-<div id="container">
-	<h1>Welcome to Carrito!</h1>
 
-</div>
-
-</body>
-</html> -->
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>bootstrap/css/bootstrap.min.css">
-    <script src="<?php echo base_url(); ?>bootstrap/js/bootstrap.bundle.min.js"></script>
-</head>
-<body>
-<nav class="navbar bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand">Navbar</a>
-    <form class="d-flex" role="search">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
 <div class="alert alert-primary" role="alert">
   A simple primary alert—check it out!
 </div>
@@ -60,7 +24,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div class="alert alert-dark" role="alert">
   A simple dark alert—check it out!
 </div>
-    <script src="<?php echo base_url(); ?>bootstrap/js/bootstrap.min.js"></script>
-    
-</body>
-</html>
+ 
--- web/application/views/inc/menu.php
+++ web/application/views/inc/menu.php
@@ -6,7 +6,7 @@
 
     </div>
 
-    <a class="navbar-brand col-4 col-lg-2 " href="#"><img class="me-3" width="auto;"
+    <a class="navbar-brand col-4 col-lg-2 " href="<?php echo base_url(); ?>index.php/web/index"><img class="me-3" width="auto;"
     height=" 50px" src="<?php echo base_url(); ?>/img/logo_madex.png" alt=""></a>
 
     <button class="navbar-toggler col-9 d-lg-none " type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,13 +18,16 @@
        
         
             <li class="nav-item col me-4 ">
-            <a class="nav-link fs-5 link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover fw-bolder" aria-hidden="true"  href="#">Productos</a>
+            <a class="nav-link fs-5 link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover fw-bolder" aria-hidden="true"  
+            href="<?php echo base_url(); ?>index.php/web/producto">Productos</a>
             </li>
             <li class="nav-item col me-4">
-            <a class="nav-link fs-5 link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover fw-bolder" aria-current="page"  href="#">Carrito</a>
+            <a class="nav-link fs-5 link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover fw-bolder" aria-current="page"  
+            href="<?php echo base_url(); ?>index.php/web/carrito ">Carrito</a>
             </li>
             <li class="nav-item fs-5 col me-4">
-            <a class="nav-link link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover fw-bolder" aria-current="page"  href="#">Contacto</a>
+            <a class="nav-link link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover fw-bolder" aria-current="page"  
+            href="<?php echo base_url(); ?>index.php/web/acerca ">Contacto</a>
             </li>
 
       </ul>
